5dae016afbb6ae9ec9392c082545cbf4c01b116e
WisWho <Yusufmuhibuddin3@gmail.com>
2025-10-21 13:13:49 +0700

    Ucup

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const sapaButton = document.getElementById('sapaButton');
 
 sapaButton.addEventListener('click', function () {
+    // alert("Hao, Selamat Datang di Profil Saya!");
     Swal.fire('Halo, Selamat Datang di Profil Saya!');
 });
